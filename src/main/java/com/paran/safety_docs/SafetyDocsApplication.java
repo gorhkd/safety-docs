1d7b17c4e17e88fb983620e146069756d6d564d3
gorhkd <git@github.com:gorhkd/safety-docs.git>
2026-07-13 15:06:22 +0900

    Initial commit

---
+++ src/main/java/com/paran/safety_docs/SafetyDocsApplication.java
@@ -0,0 +1,13 @@
+package com.paran.safety_docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyDocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paran/safety_docs/SafetyDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paran.safety_docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

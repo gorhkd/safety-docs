13b83a0d53f6791784352c5c3661929ada6a916d
gorhkd <gorhkd@gmail.com>
2026-07-14 16:06:28 +0900

    feat: user 계정 상태 추가(Active, Inactive)

---
+++ src/main/java/com/paran/safety_docs/domain/auth/entity/ActiveStatus.java
@@ -0,0 +1,5 @@
+package com.paran.safety_docs.domain.auth.entity;
+
+public enum ActiveStatus {
+    ACTIVE, INACTIVE
+}
--- src/main/java/com/paran/safety_docs/domain/auth/entity/User.java
+++ src/main/java/com/paran/safety_docs/domain/auth/entity/User.java
@@ -1,14 +1,17 @@
 package com.paran.safety_docs.domain.auth.entity;
 
 import com.paran.safety_docs.global.common.entity.BaseCreatedOnlyEntity;
+import com.paran.safety_docs.global.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Table(name = "users")
 @Entity
-public class User extends BaseCreatedOnlyEntity {
+public class User extends BaseEntity {
 
     private String username;
 
     private String password;
+
+    private ActiveStatus is_active;
 }

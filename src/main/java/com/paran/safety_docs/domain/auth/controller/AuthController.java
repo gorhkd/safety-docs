cfd02cf9a5f7528238525ce72a80e1e7135c4676
gorhkd <gorhkd@gmail.com>
2026-07-24 09:21:29 +0900

    feat: AdminAccountController, login api 구현

--- src/main/java/com/paran/safety_docs/domain/auth/controller/AuthController.java
+++ src/main/java/com/paran/safety_docs/domain/auth/controller/AuthController.java
@@ -2,8 +2,15 @@ package com.paran.safety_docs.domain.auth.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class AuthController {
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "admin/login";
+    }
+
 }
